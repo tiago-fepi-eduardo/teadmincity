3db41be004fbfce3f06e633687c394956cfd8baf
teduardo <tiago.eduardo@capgemini.com>
2021-02-27 12:47:10 -0300

    New pages created. Homepage structure.

--- teadmincity/te-admin-city/src/app/app.component.ts
+++ teadmincity/te-admin-city/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'app-root',
+  template: `
+  <app-header></app-header>
+  <app-home></app-home>
+  <app-footer></app-footer>
+  `
 })
 export class AppComponent {
-  title = 'te-admin-city';
+  title = 'Admin City';
 }
